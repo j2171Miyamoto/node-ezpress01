b8ee67a5cb82d650ed95d29aa56b6e9f2a129b9c
j2171Miyamoto <166687789+j2171Miyamoto@users.noreply.github.com>
2024-05-16 07:00:58 +0000

    Express.jsの学習

--- index.js
+++ index.js
@@ -2,10 +2,36 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// GETリクエスト
 app.get('/', (req, res) => {
-  res.send('Hello World!松江高専万歳‼gameproムズイ')
-})
+  res.send({
+    msg:'GET request'
+  });
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// POSTリクエスト
+app.post('/', (req, res) => {
+  res.send({
+    msg:'POST request'
+  });
+});
+
+// PUTリクエスト
+app.put('/:id', (req, res) => {
+  res.send({
+    id: req.params.id,
+    msg:'PUT request'
+  });
+});
+
+// DELETEリクエスト
+app.delete('/:id', (req, res) => {
+  res.send({
+    id: req.params.id,
+    msg:'DELETE request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
