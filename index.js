f68e963ee7906a2e5b0beda05162eb0e3507eb3d
j2171Miyamoto <166687789+j2171Miyamoto@users.noreply.github.com>
2024-05-16 07:11:35 +0000

    pub

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
-const app = express()
+const app = express();
+const path = require('path');
 const port = 3000
+
+// 静的ファイルの提供とキャッシュの有効化
+app.use(express.static('public', { maxAge: 86400000 }));
+
+// test-imageリクエスト
+app.get('/test-image', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/images/test.png'));
+});
 
 // GETリクエスト
 app.get('/', (req, res) => {
